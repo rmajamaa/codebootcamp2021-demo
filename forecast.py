affea578b6092dffa8def5016b63d97423074748
Risto Majamaa <rmajamaa@gmail.com>
2021-02-06 05:42:23 +0200

    Increased message interval and added break command

--- forecast.py
+++ forecast.py
@@ -57,4 +57,5 @@ for weather in weather_list[0:5]:
 
     # Sends the message at selected intervals (time in seconds), e.g one hour = 3600, three hours = 10800
     requests.get(telegram_bot_url)
-    time.sleep(3600)
+    time.sleep(10800)
+    break
